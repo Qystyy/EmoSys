6cd2dc18cca1661a581e887b577c0763eafbaedf
Qystyy <husmizamaina@gmail.com>
2026-09-11 14:48:07 +0800

    rename file

--- model/notebook/Gesture_model_training.py
+++ model/notebook/Gesture_model_training.py
@@ -3,7 +3,7 @@ Gesture Model Training - updated 11/9/2026
 
 Usage:
 - Model training using gesture data extracted using mp_gesture_extractor.py
-- Model was created from scratch without the use of transfer learning 
+- CCN model without the use of transfer learning 
 
 """
 
