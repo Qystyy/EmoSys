83453f94c138603f700072bd57f5930cb12304f6
Qystyy <husmizamaina@gmail.com>
2026-09-11 09:36:51 +0800

    update push module

--- code/push_module.py
+++ code/push_module.py
@@ -1,11 +1,36 @@
+"""
+Push Module - updated 10/9/2026
+
+Usage:
+- Connection with the main dashboard to POST data from raspberry Pi 5
+- Data being push:
+    - Emotions
+    - Emotions confidence level
+    - Gesture 
+    - Gesture confidence level
+- Device ID is the ID of current raspberry Pi 5
+- The url is the Dashboard URL; where the data will be outputted
+- The Token is the restAPI to send data to raspberry Pi [Dashboard]
+- The data will later be stored in the Main Dashboard InfluxDB
+- The image from the Raspberry Pi will be deleted immediately after being push to the dashboard
+- While in the dashboard, the image will be in cache form and deleted after 30 minutes
+
+To paste in the inference code:
+from push_module import push_result
+
+push_result(detected_emotion, confidence_score, frame_jpeg_bytes, inf_speed, gesture_cf, gesture_label)
+# the name in the () is based on what you assign in the code
+"""
+
+
 import requests
 from enum import Enum
 
 PI5_URL = "http://10.0.30.7:3000/api/emotion/ingest"
 TOKEN   = "e591962c78716e9fbd2677d2125b2375"
 
-#def push_result (emotion, confidence, frame_jpeg_bytes, inference_speed_ms, posture_score, posture_label, habit_label, habit_cf):
-def push_result (emotion, confidence, frame_jpeg_bytes, inference_speed_ms, gesture_score, gesture_label):
+
+def push_result (emotion, confidence, frame_jpeg_bytes, inference_speed_ms, gesture, gesture_score):
     files = {
         'image': ('frame.jpg', frame_jpeg_bytes, 'image/jpeg')
         }
@@ -13,12 +38,8 @@ def push_result (emotion, confidence, frame_jpeg_bytes, inference_speed_ms, gest
         'emotion'          : emotion,
         'confidence'       : confidence,
         'inferenceSpeedMs' : inference_speed_ms,
+        'gestureLabel'     : gesture,
         'gestureScore'     : gesture_score,
-        'gestureLabel'     : gesture_label,
-        #'postureScore'     : posture_score,
-        #'postureLabel'     : posture_label,
-        #'habitLabel'       : habit_label.value if isinstance(habit_label, Enum) else habit_label,
-        #'habitScore'       : habit_cf,
         'deviceId'         : 'pi_1',
         'modelVersion'     : 'v1'
     } 
@@ -30,7 +51,4 @@ def push_result (emotion, confidence, frame_jpeg_bytes, inference_speed_ms, gest
         print(f"Pushed: {r.status_code} {r.text}")
     except requests.RequestException as e:
         print(f"Push failed: {e}")
-
-#push_result(detected_emotion, confidence_score, current_frame_jpeg_bytes)
-# call for send frames to server
 
